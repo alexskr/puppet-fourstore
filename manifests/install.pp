cbdcac009299b3cafa5cd715d489977c772cdae6
Alex Skrenchuk <alexskr@stanford.edu>
2021-06-25 14:10:59 -0700

    PDK v2.1.1 update

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,36 +12,26 @@
 # Sample Usage:
 #
 class fourstore::install(
-  $raptor2_ver = '2.0.15',
-  $rasqal_ver  = '0.9.33',
-  $fsrepo      = 'https://github.com/ncbo/4store',
-  $fsrevision  = 'master',
-  $data_dir    = '/srv/4store'
+  String $fsrepo                 = 'https://github.com/ncbo/4store',
+  String $fsrevision             = 'master',
+  Stdlib::Absolutepath $data_dir = '/srv/4store'
 ) {
 
-  require librdf::rasqal
   include git
+  require fourstore::dependencies
+  require librdf::rasqal
 
   user { '4store':
-  ensure           => 'present',
-  system           => true,
-  comment          => '4Store Server',
-  home             => '/var/lib/4store',
-  password         => '!!',
-  password_max_age => '-1',
-  password_min_age => '-1',
-  shell            => '/bin/bash',
-  #uid             => '70009',
-  #gid             => '70009',
-}
-# group { '4store':
-#   ensure => 'present',
-#   gid    => '70009',
-# }
-
-
-  if ! defined( Class['fourstore::dependencies'] ) {
-    require fourstore::dependencies
+    ensure           => 'present',
+    system           => true,
+    comment          => '4Store Server',
+    home             => '/var/lib/4store',
+    password         => '!!',
+    password_max_age => '-1',
+    password_min_age => '-1',
+    shell            => '/bin/bash',
+    #uid             => '70009',
+    #gid             => '70009',
   }
 
   #ulimits
@@ -51,7 +41,7 @@ class fourstore::install(
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/fourstore/limits.d-4store.conf',
-}
+  }
 
   #data directory
   file { $data_dir :
@@ -81,11 +71,11 @@ class fourstore::install(
     mode   => '0755',
   }
 
-  file {  '/usr/local/bin/fixperms':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
+  file {  '/usr/local/bin/4s-fixperms':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
     content => "chown -R 4store:4store ${data_dir}",
   }
 
@@ -96,21 +86,20 @@ class fourstore::install(
     revision => $fsrevision,
   }
 
-  exec { 'make_4store':
-    command => '/usr/local/bin/make4store deps',
-    cwd     => '/usr/local/bin/',
-    creates => '/usr/local/bin/4s-backend',
-    timeout => 1200,
-    require =>File['/usr/local/bin/make4store'],
-  }
-
   file {  '/usr/local/bin/make4store':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
-    content  => epp('fourstore/make4store.epp', { 'data_dir' => $data_dir }),
+    content => epp('fourstore/make4store.epp', { 'data_dir' => $data_dir }),
     require => Vcsrepo['/usr/local/src/4store'],
   }
 
+  exec { 'make_4store':
+    command => '/usr/local/bin/make4store deps',
+    cwd     => '/usr/local/bin/',
+    creates => '/usr/local/bin/4s-backend',
+    timeout => 1200,
+    require => File['/usr/local/bin/make4store'],
+  }
 }
